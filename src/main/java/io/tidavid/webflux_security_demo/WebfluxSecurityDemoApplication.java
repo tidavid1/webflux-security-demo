a536ef8bac2f2f7a9a3615d2c4e6c8653707db83
KyungMin Lee <hello.tidavid@gmail.com>
2025-01-05 22:19:59 +0900

    init: Initial Commit

---
+++ src/main/java/io/tidavid/webflux_security_demo/WebfluxSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package io.tidavid.webflux_security_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxSecurityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxSecurityDemoApplication.class, args);
+    }
+
+}
